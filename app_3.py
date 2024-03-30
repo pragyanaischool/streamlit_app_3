8b6d40a36dfce4fe8040392b398b199bf896f8b3
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2024-03-31 01:03:29 +0530

    Update app_3.py

--- app_3.py
+++ app_3.py
@@ -2,8 +2,25 @@
 # See LICENSE for details.
 
 import tensorflow as tf
+import streamlit as st
+import requests
 
-from constants import MAPPING
+# TF-Serving URLs for both gRPC and REST APIs
+GRPC_URL = "tfserving:8500"
+REST_URL = "http://tfserving:8501/v1/models/simpsonsnet:predict"
+
+# Mapping of ids to labels (The Simpsons characters)
+MAPPING = {
+    0: "abraham_grampa_simpson", 1: "apu_nahasapeemapetilon", 2: "barney_gumble", 3: "bart_simpson",
+    4: "carl_carlson", 5: "charles_montgomery_burns", 6: "chief_wiggum", 7: "comic_book_guy",
+    8: "disco_stu", 9: "edna_krabappel", 10: "groundskeeper_willie", 11: "homer_simpson",
+    12: "kent_brockman", 13: "krusty_the_clown", 14: "lenny_leonard", 15: "lisa_simpson",
+    16: "maggie_simpson", 17: "marge_simpson", 18: "martin_prince", 19: "mayor_quimby",
+    20: "milhouse_van_houten", 21: "moe_szyslak", 22: "ned_flanders", 23: "nelson_muntz",
+    24: "patty_bouvier", 25: "principal_skinner", 26: "professor_john_frink", 27: "ralph_wiggum",
+    28: "selma_bouvier", 29: "sideshow_bob", 30: "snake_jailbird", 31: "waylon_smithers"
+}
+
 
 def image2tensor(image_as_bytes):
     """
@@ -34,24 +51,6 @@ def prediction2label(prediction):
     prediction = tf.argmax(prediction)
     return MAPPING[prediction.numpy()]
 
-# TF-Serving URLs for both gRPC and REST APIs
-GRPC_URL = "tfserving:8500"
-REST_URL = "http://tfserving:8501/v1/models/simpsonsnet:predict"
-
-# Mapping of ids to labels (The Simpsons characters)
-MAPPING = {
-    0: "abraham_grampa_simpson", 1: "apu_nahasapeemapetilon", 2: "barney_gumble", 3: "bart_simpson",
-    4: "carl_carlson", 5: "charles_montgomery_burns", 6: "chief_wiggum", 7: "comic_book_guy",
-    8: "disco_stu", 9: "edna_krabappel", 10: "groundskeeper_willie", 11: "homer_simpson",
-    12: "kent_brockman", 13: "krusty_the_clown", 14: "lenny_leonard", 15: "lisa_simpson",
-    16: "maggie_simpson", 17: "marge_simpson", 18: "martin_prince", 19: "mayor_quimby",
-    20: "milhouse_van_houten", 21: "moe_szyslak", 22: "ned_flanders", 23: "nelson_muntz",
-    24: "patty_bouvier", 25: "principal_skinner", 26: "professor_john_frink", 27: "ralph_wiggum",
-    28: "selma_bouvier", 29: "sideshow_bob", 30: "snake_jailbird", 31: "waylon_smithers"
-}
-
-import streamlit as st
-import requests
 
 # General information about the UI
 st.title("TensorFlow Serving + Streamlit! ✨🖼️")
